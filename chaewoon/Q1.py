ad7d98088c1a2e362c15d7a175c1683c730585ad
3hwooni <3hwooni@gmail.com>
2020-05-03 16:33:42 +0900

    Week 1

---
+++ chaewoon/Q1.py
@@ -0,0 +1,15 @@
+def sum_n1(n):
+    s=0
+    for i in range(1,n+1):
+        s = s + i
+    return s
+
+def sum_n2(n):
+    return n*(1+n)/2
+
+def sum_nn(n):
+    s = 0
+    for i in range(1,n+1):
+        s = s + i*i
+    return s
+
